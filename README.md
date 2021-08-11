d50227e31c089f600759bb4935167767f7a0eb44
ZYA <zyazhb@yahoo.com>
2021-08-11 20:44:38 +0800

    Update install script

---
+++ README.md
@@ -0,0 +1,9 @@
+# OH ZYA TERMINAL
+
+## What's this
+
+My Personal Terminal config for efficiency work.  
+
+## How to Use
+
+Just run install and enjoy.   
--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
 apt update
 apt install curl git zsh tmux -y
+if test -z $ZSH; then
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-ln -s -f ./.zshrc ./.tmux.conf ./.tmux.conf.local ~/
+fi
+ln -s -f $(pwd)/.zshrc ~/.zshrc
+ln -s -f $(pwd)/.tmux.conf ~/.tmux.conf
+ln -s -f $(pwd)/.tmux.conf.local ~/.tmux.conf.local
